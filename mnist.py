4429b16f4763a759eca00e3b5c439c2e9c507edb
lam-01 <105969643+lam-01@users.noreply.github.com>
2025-03-05 01:23:10 +0700

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -203,7 +203,7 @@ def create_streamlit_app():
                 filtered_runs = runs
 
             if not filtered_runs.empty:
-                st.dataframe(filtered_runs[["model_custom_name", "model_name","run_id", "start_time", "metrics.train_accuracy", "metrics.val_accuracy", "metrics.test_accuracy"]])
+                st.dataframe(filtered_runs[["model_custom_name","run_id", "start_time", "metrics.train_accuracy", "metrics.val_accuracy", "metrics.test_accuracy"]])
 
                 selected_run_id = st.selectbox("📝 Chọn một mô hình để xem chi tiết:", filtered_runs["run_id"].tolist())
                 if selected_run_id:
